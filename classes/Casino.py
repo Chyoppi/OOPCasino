dafd8a3ff45bc02073628441a30bb231db5bddf4
ChrisM <chrisu.mannik@hotmail.com>
2025-04-01 11:21:52 +0300

    Modifications to casino file and removed poker and added slot

--- classes/Casino.py
+++ classes/Casino.py
@@ -1,3 +1,18 @@
+#Games which are commented have not been implemented yet
+
+#from games.Blackjack import Blackjack
+#from games.Slot import Slot
+#from games.Roulette import Roulette
+from games.DiceGame import DiceGame
+from Player import Player
+
+
+
 class Casino:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, balance:int, dicegame:DiceGame):
+        self.balance = balance
+        self.dicegame = dicegame
+        #self.games = {}
+
+    def __str__(self):
+        return f"Welcome to casino, you have {self.balance} points in your balance"
